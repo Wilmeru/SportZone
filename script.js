57cb24bc0f9d5a16c863d97c9aba73b0a3d00388
ThomasAriza1227 <arizathomas2007@gmail.com>
2026-08-11 08:40:30 -0500

    Se creo el archivo de JavaScript y se agrego las variables de productos

---
+++ script.js
@@ -0,0 +1,71 @@
+/* =========================
+   PRODUCTOS
+========================= */
+
+let productos = [
+
+    {
+        id: 1,
+        nombre: "Tenis Running Pro",
+        categoria: "Calzado",
+        precio: 199900,
+        imagen: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=500&q=80"
+    },
+
+    {
+        id: 2,
+        nombre: "Camiseta Deportiva",
+        categoria: "Ropa",
+        precio: 69900,
+        imagen: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?auto=format&fit=crop&w=500&q=80"
+    },
+
+    {
+        id: 3,
+        nombre: "Balón de Fútbol",
+        categoria: "Balones",
+        precio: 129900,
+        imagen: "https://images.unsplash.com/photo-1614632537190-23e4146777db?auto=format&fit=crop&w=500&q=80"
+    },
+
+    {
+        id: 4,
+        nombre: "Maleta Deportiva",
+        categoria: "Accesorios",
+        precio: 149900,
+        imagen: "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?auto=format&fit=crop&w=500&q=80"
+    },
+
+    {
+        id: 5,
+        nombre: "Guantes de Arquero",
+        categoria: "Accesorios",
+        precio: 89900,
+        imagen: "https://images.unsplash.com/photo-1579952363873-27f3bade9f55?auto=format&fit=crop&w=500&q=80"
+    },
+
+    {
+        id: 6,
+        nombre: "Gorra Deportiva",
+        categoria: "Ropa",
+        precio: 39900,
+        imagen: "https://images.unsplash.com/photo-1588850561407-ed78c282e89b?auto=format&fit=crop&w=500&q=80"
+    },
+
+    {
+        id: 7,
+        nombre: "Mancuernas",
+        categoria: "Accesorios",
+        precio: 79900,
+        imagen: "https://images.unsplash.com/photo-1581009146145-b5ef050c2e1e?auto=format&fit=crop&w=500&q=80"
+    },
+
+    {
+        id: 8,
+        nombre: "Balón de Baloncesto",
+        categoria: "Balones",
+        precio: 99900,
+        imagen: "https://images.unsplash.com/photo-1546519638-68e109498ffc?auto=format&fit=crop&w=500&q=80"
+    }
+
+];
